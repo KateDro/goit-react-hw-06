738ea892a81f72c21eddd290048c7eb6fa1d738e
KateDro <106846150+KateDro@users.noreply.github.com>
2024-02-22 18:13:32 +0100

    upd

--- src/redux/numberSlice.js
+++ src/redux/numberSlice.js
@@ -5,10 +5,8 @@ const initialState = [
   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  { id: "id-5", name: "Andrian Pan", number: "459444-12-56" },
-  { id: "id-6", name: "Yura Robin", number: "44124-839-12" },
-  { id: "id-7", name: "Ira Comandik", number: "64324-14237-79" },
-  { id: "id-8", name: "Diana TopGun", number: "22567-941-26" },
+  { id: "id-5", name: "Andrian Smith", number: "459444-12-56" },
+  { id: "id-6", name: "Jack Berry", number: "44124-839-12" },
 ];
 
 const numberSlice = createSlice({
